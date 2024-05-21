54177dbb3a63ed02ac027c6e63d45cd4fec39c6e
Seeyong <seeyong987@gmail.com>
2024-05-21 20:13:53 +0900

    apply nestjs config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const port: number = 3000;
+  const port: number = parseInt(process.env.PORT);
   await app.listen(port);
   console.log(`Listening on port: ${port}`);
 }

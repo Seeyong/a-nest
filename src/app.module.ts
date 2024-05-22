859323cfc244373d58a6e4358e2df9fa97547bdb
Seeyong <seeyong987@gmail.com>
2024-05-22 12:49:08 +0900

    add services and controllers for users and dms

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from './logger.middleware';
+import { UsersModule } from './users/users.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get()
+  getUsers(@Req() req) {
+    return req.user;
+  }
+
+  @Post()
+  postUsers(@Body() body: JoinRequestDto) {
+    this.usersService.postUsers(body.email, body.nickname, body.password);
+  }
+
+  @Post('login')
+  logIn(@Req() req) {
+    return req.user;
+  }
+
+  @Post('logout')
+  logOut(@Req() req, @Res() res) {
+    req.logOut();
+    res.clearCookie('connet.sid', { httpOnly: true });
+    res.send('ok');
+  }
+}

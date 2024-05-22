d2bdde4c570daa02eb1ce8b7024b8c4c2a5f004f
Seeyong <seeyong987@gmail.com>
2024-05-22 10:22:26 +0900

    app use cookieParser passport session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const port: number = parseInt(process.env.PORT);
+  app.use(cookieParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(port);
   console.log(`Listening on port: ${port}`);
 }
